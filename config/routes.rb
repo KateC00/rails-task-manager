2cbe21e095c6b97756a94516be89758995684e20
catherine cailleret <cailleret.catherine00@gmail.com>
2021-11-16 16:58:40 +0100

    rails task manager done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get "tasks", to: "tasks#all"
-  get "tasks/new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new", as: :new
   get "tasks/:id", to: "tasks#show", as: :task
   post "tasks", to: "tasks#create"
-  get "tasks/:id/edit", to: "tasks#edit"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
   patch "tasks/:id", to: "tasks#update"
-  delete "tasks/:id", to: "tasks#destroy"
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
 end
